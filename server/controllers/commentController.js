dd6e49ff67fbd1c27b05e12d0bf220b4e3b02d3b
Yuvangates <yuvangates.dake@gmail.com>
2025-10-18 13:57:56 +0530

    delete option for individual user is added

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -49,3 +49,39 @@ exports.upvoteComment = async (req, res) => {
     res.status(500).json({ message: 'Server error upvoting comment' });
   }
 };
+
+const findAllChildrenIds = async (commentId) => {
+  let childrenIds = [];
+  const directChildren = await Comment.find({ parent: commentId }).select('_id');
+  if (directChildren.length > 0) {
+    childrenIds = directChildren.map(c => c._id);
+    for (const childId of childrenIds) {
+      const grandChildrenIds = await findAllChildrenIds(childId);
+      childrenIds = childrenIds.concat(grandChildrenIds);
+    }
+  }
+  return childrenIds;
+};
+
+
+exports.deleteComment = async (req, res) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+
+    if (comment.user.toString() !== req.user) {
+      return res.status(401).json({ message: 'User not authorized' });
+    }
+    
+    const commentsToDelete = [comment._id, ...await findAllChildrenIds(comment._id)];
+    
+    await Comment.deleteMany({ _id: { $in: commentsToDelete } });
+
+    res.json({ message: 'Comment and replies deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error deleting comment' });
+  }
+};
--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAllComments, createComment, upvoteComment } = require('../controllers/commentController');
+const { getAllComments, createComment, upvoteComment, deleteComment } = require('../controllers/commentController');
 const { protect } = require('../middleware/authMiddleware'); 
 
 router.get('/', getAllComments);
 
 router.post('/', protect, createComment);
 router.patch('/:id/upvote', protect, upvoteComment);
+router.delete('/:id', protect, deleteComment);
 
 module.exports = router;
